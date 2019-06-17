09d03a4f3018ae558e77b3ccd248f046993d50f8
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-06-17 14:49:21 -0700

    ⛓ added urls to main app

--- networq/networq/urls.py
+++ networq/networq/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    # path('cards/', include(cards.urls)),
+    path('admin/', admin.site.urls),
 ]
