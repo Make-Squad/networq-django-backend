ff6e7941b996e0c8393d8d18f31d2498490328a6
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-06-26 15:27:32 -0700

    add homepage tests using simpletestcase

--- networq/cards/tests.py
+++ networq/cards/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.http import HttpRequest
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 #user should be able to login if they have not registered
 
@@ -36,3 +38,26 @@ class UsersTest(TestCase):
     with self.assertRaises(ValueError):
       User.objects.create_user(email='', password='password')
   
+class HomePageTests(SimpleTestCase):
+  
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, '<h1>Homepage</h1>')
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
