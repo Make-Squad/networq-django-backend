d89858f871864f78189f8330d8bccb261832b059
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-06-26 14:07:12 -0700

    😊 working on user model

--- networq/users/models.py
+++ networq/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=25, required=True)
+    last_name = models.CharField(max_length=25, required=True)
+
+    email = models.EmailField(min_length=4, required=True)
+    
+    city = models.CharField(min_length=2, max_length=50)
+    # ask user to input state code e.g. CA for california, GA for georgia
+    state = models.CharField(max_length=3)
+
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, required=True) # validators should be a list
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- networq/users/urls.py
+++ networq/users/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /users/settings - edit user settings 
+    
+    # user's dashboard/profile page 
     url('users/', views.users, name='users'),
+
     url(r'', views.index, name='index')
     ]
