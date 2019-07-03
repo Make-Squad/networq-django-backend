702a95d5044aa8bf11786fe3ee03aeb65a9583c2
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-07-03 13:11:34 -0700

    did a shit ton of work

--- networq/settings.py
+++ networq/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,4 +130,8 @@ STATIC_URL = '/static/'
 
 APPEND_SLASH = True
 
-django_heroku.settings(locals())
+try: 
+    import django_heroku
+    django_heroku.settings(locals())
+except: 
+    pass 
